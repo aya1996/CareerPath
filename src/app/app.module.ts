e1a8879dd542e8487170420b779f1f55d07e532f
aya_megahed <aya.mohamed666fciscu@gmail.com>
2020-06-03 03:36:02 +0200

    create navbar+footer+home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes }from "@angular/router";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+// import {NgbdCollapseNavbar} from './collapse-navbar';
 
+
+const routes:Routes = 
+[
+ 
+  {  path:'home',  component : HomeComponent  },
+
+  {  path:'**',  component : HomeComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
