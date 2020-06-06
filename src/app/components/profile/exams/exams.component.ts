e366da87eea2e861a388c4eadca3ffb190206439
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2020-06-06 13:39:02 +0200

    profile-exams

--- src/app/components/profile/exams/exams.component.ts
+++ src/app/components/profile/exams/exams.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamsComponent implements OnInit {
 
+  items = [1, 2, 3, 4];
+  
+  courseName:String = 'Typescript';
+  testDuration:Number = 15;
+  testDate:String = '15/5/2020';
+  grade:Number = 17;
+  
   constructor() { }
 
   ngOnInit() {
