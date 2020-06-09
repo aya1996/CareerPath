094254d2b3a7a30c889848bfb6647f85087c1b13
ahmed1993-ops <ahmedsarhan.dev@gmail.com>
2020-06-09 08:41:32 -0700

    secound comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,56 +3,52 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { RouterModule, Routes }from "@angular/router";
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { RouterModule, Routes } from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfileHeaderComponent } from './components/profile/profile-header/profile-header.component';
 import { DashboardComponent } from './components/profile/dashboard/dashboard.component';
 import { CoursesComponent } from './components/profile/courses/courses.component';
 import { ExamsComponent } from './components/profile/exams/exams.component';
 import { DoughnuComponent } from './components/profile/charts/doughnu/doughnu.component';
-
 import { ChartsModule } from 'ng2-charts';
 import { CourseModalComponent } from './components/profile/course-modal/course-modal.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { CoursesListComponent } from './components/courses/courses-list/courses-list.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSnackBarModule} from '@angular/material';
+import { MatSnackBarModule } from '@angular/material';
 
->>>>>>> b08da560f594ec9c35b6a9a65321647974ab546d
+import { RegisterComponent } from './pages/register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { WebDevelopmentComponent } from './pages/web-development/web-development.component';
+import { CourseComponent } from './pages/course/course.component';
+import { LoginComponent } from './pages/login/login.component';
+
 // import {NgbdCollapseNavbar} from './collapse-navbar';
 
 
-const routes:Routes = 
-[
- 
-  { path:'',  component : HomeComponent  },
+const routes: Routes =
+  [
 
-  { path:'profile', component: ProfileHeaderComponent},
+    { path: '', component: HomeComponent },
 
-  { path:'profile/dashboard', component: DashboardComponent},
+    { path: 'profile', component: ProfileHeaderComponent },
 
-  { path:'profile/courses', component: CoursesComponent},
+    { path: 'profile/dashboard', component: DashboardComponent },
 
-  { path:'courses-list', component:CoursesListComponent }
+    { path: 'profile/courses', component: CoursesComponent },
 
-  // {  path:'**',  component : HomeComponent }
-]
+    { path: 'courses-list', component: CoursesListComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'web', component: WebDevelopmentComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'course', component: CourseComponent },
+    // {  path:'**',  component : HomeComponent }
+  ]
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    NgbModule,
-    ChartsModule,
-    AngularFontAwesomeModule,
-    NoopAnimationsModule,
-    MatSnackBarModule
-  ],
+
   declarations: [
     AppComponent,
     NavbarComponent,
@@ -63,19 +59,26 @@ const routes:Routes =
     CoursesComponent,
     ExamsComponent,
     DoughnuComponent,
-<<<<<<< HEAD
-    CourseModalComponent
+    CourseModalComponent,
+    LoginComponent,
+    RegisterComponent,
+    WebDevelopmentComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     NgbModule,
     ChartsModule,
-    BrowserAnimationsModule
-=======
-    CourseModalComponent,
-    CoursesListComponent
->>>>>>> b08da560f594ec9c35b6a9a65321647974ab546d
+    BrowserAnimationsModule,
+    CoursesListComponent,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    NoopAnimationsModule,
+    AngularFontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
