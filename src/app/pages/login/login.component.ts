879397a086ad812d3faeac35491f89b5376a7e0c
ahmed1993-ops <ahmedsarhan.dev@gmail.com>
2020-06-09 08:27:38 -0700

    first comment

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) { }
+}
