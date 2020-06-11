afe7dd3c8df06e482e31526fbb2a9fda646555d2
ahmed maher <ahmedsarhan.dev@gmail.com>
2020-06-11 21:30:40 +0200

    course service comment

--- src/app/shared/services/register.service.ts
+++ src/app/shared/services/register.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class RegisterService {
   modelName = 'register';
   constructor(
     private http: HttpClient,
