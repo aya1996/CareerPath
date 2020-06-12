b219354b1be6dee81ab6e197e38f7593f1744fc5
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2020-06-12 02:48:53 +0200

    adding user

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +11,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [
+    {value: '0', viewValue: 'Gin'},
+    {value: '1', viewValue: 'Rem'},
+    {value: '2', viewValue: 'Akai'}
+  ];
+
+  displayedColumns: string[] = ['Course', 'Score', 'Time', 'Examdate','examQs'];
+  dataSource: MatTableDataSource<Exam>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor() {
+
+    this.dataSource = new MatTableDataSource(EXAMS);
+  }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
+
+
+interface Exam {
+  course: string,
+  score: number,
+  time: number,
+  examDate: string,
+}
+const EXAMS: Exam[] = [
+  {
+    course: 'HTML',
+    score: 17,
+    time: 20,
+    examDate: '15/5/2020'
+  },
+  {
+    course: 'CSS',
+    score: 19,
+    time: 20,
+    examDate: '15/5/2020'
+  },
+  {
+    course: 'Javascript',
+    score: 15,
+    time: 20,
+    examDate: '15/5/2020'
+  },
+  {
+    course: 'Javascript',
+    score: 15,
+    time: 20,
+    examDate: '15/5/2020'
+  },
+  {
+    course: 'Javascript',
+    score: 15,
+    time: 20,
+    examDate: '15/5/2020'
+  },
+  {
+    course: 'Javascript',
+    score: 15,
+    time: 20,
+    examDate: '15/5/2020'
+  }
+  
+  
+];
+
+interface User {
+  value: string;
+  viewValue: string;
+}
+
+
+
